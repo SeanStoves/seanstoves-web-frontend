495e000906521acb961ea2a2cbf24976c3b8d892
Sean Stoves <sstoves@fandom.com>
2021-08-31 16:14:08 -0400

    Updating Footer to have Social Icons and proper alignment.

--- components/footer.js
+++ components/footer.js
@@ -1,21 +1,38 @@
 import styles from "../styles/Footer.module.css"
-import {Navbar, Nav, NavDropdown} from "react-bootstrap";
+import {Col, Container, Row} from "react-bootstrap";
+import {
+    FaTwitter,
+    FaTwitch,
+    FaGithub,
+    FaFacebookF,
+    FaLinkedinIn,
+    FaDAndD,
+    FaSteam
+} from "react-icons/fa";
 
 export default function Footer() {
     return (
         <footer className={styles.footer}>
             <hr />
-            <Navbar sticky="bottom" collapseOnSelect expand="lg" bg="dark" variant="dark">
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="https://twitter.com/sstoves">Twitter</Nav.Link>
-                        <Nav.Link href="https://www.facebook.com/sstoves">Facebook</Nav.Link>
-                        <Nav.Link href="https://github.com/SeanStoves">GitHub</Nav.Link>
-                        <Nav.Link href="https://www.linkedin.com/in/seanstoves">LinkedIn</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
+            <Container fluid>
+                <Row>
+                    <Col></Col>
+                    <Col></Col>
+                    <Col></Col>
+                    <Col></Col>
+                    <Col>
+                        <ul className={styles.SocialNetwork}>
+                            <li><a target="_blank" href="https://www.dndbeyond.com/members/SolaoBajiuik"><FaDAndD /></a></li>
+                            <li><a target="_blank" href="https://www.facebook.com/sstoves"><FaFacebookF /></a></li>
+                            <li><a target="_blank" href="https://www.linkedin.com/in/seanstoves"><FaLinkedinIn /></a></li>
+                            <li><a target="_blank" href="https://twitter.com/sstoves"><FaTwitter /></a></li>
+                            <li><a target="_blank" href="https://twitch.tv/solaobajiuik"><FaTwitch /></a></li>
+                            <li><a target="_blank" href="https://github.com/SeanStoves"><FaGithub /></a></li>
+                            <li><a target="_blank" href="https://steamcommunity.com/id/solaobajiuik"><FaSteam /></a></li>
+                        </ul>
+                    </Col>
+                </Row>
+            </Container>
         </footer>
     )
 }
