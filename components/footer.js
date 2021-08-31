9f7d6d30ba3290301df8f343d40f5a8d6824c368
Sean Stoves <sstoves@fandom.com>
2021-08-31 17:41:07 -0400

    Update Social Media Icons

--- components/footer.js
+++ components/footer.js
@@ -26,12 +26,9 @@ export default function Footer() {
                             <li><a target="_blank" href="https://www.linkedin.com/in/seanstoves"><FaLinkedinIn /></a></li>
                             <li><a target="_blank" href="https://twitter.com/sstoves"><FaTwitter /></a></li>
                             <li><a target="_blank" href="https://github.com/SeanStoves"><FaGithub /></a></li>
-                        </ul>
-                        <ul className={styles.SocialNetwork}>
                             <li><a target="_blank" href="https://www.dndbeyond.com/members/SolaoBajiuik"><FaDAndD /></a></li>
                             <li><a target="_blank" href="https://twitch.tv/solaobajiuik"><FaTwitch /></a></li>
                             <li><a target="_blank" href="https://steamcommunity.com/id/solaobajiuik"><FaSteam /></a></li>
-                            <li><a target="_blank" href="https://us.forums.blizzard.com/en/wow/u/lightfuri-skeram/"><FaBattleNet /></a></li>
                         </ul>
                     </Col>
                 </Row>
