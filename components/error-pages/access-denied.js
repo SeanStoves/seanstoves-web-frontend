c97bffc73cf30daffd8bd69d2ba8b790f48aece7
Sean Stoves <sstoves@fandom.com>
2021-09-01 16:03:51 -0400

    Updating for Github Actions and Docker File

--- components/error-pages/access-denied.js
+++ components/error-pages/access-denied.js
@@ -1,15 +1,16 @@
 import { signIn } from 'next-auth/client'
 import headStyle from "../../styles/Header.module.css";
+import { Link } from "next/link"
 
 export default function AccessDenied () {
     return (
         <>
             <h1>Access Denied</h1>
             <p>
-                <a className={headStyle.buttonPrimary} onSelect={(e) => {
+                <Link className={headStyle.buttonPrimary} onSelect={(e) => {
                     e.preventDefault()
                     signIn()
-                }} href="/api/auth/signin">You must be signed in to view this page</a>
+                }} href="/api/auth/signin">You must be signed in to view this page</Link>
             </p>
         </>
     )
