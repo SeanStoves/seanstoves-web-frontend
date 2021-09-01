fc731deb1fcd40d480a452b2554063c9b9fb5861
Sean Stoves <sstoves@fandom.com>
2021-09-01 12:35:08 -0400

    Adding All Contact Info to Footer. Formatting CSS For loop Social

--- components/footer.js
+++ components/footer.js
@@ -2,26 +2,26 @@ import styles from "../styles/Footer.module.css"
 import {Col, Container, Row} from "react-bootstrap";
 import React, {useEffect, useState} from "react";
 import axios from "axios";
-import {
-    FaTwitter,
-    FaTwitch,
-    FaGithub,
-    FaFacebookF,
-    FaLinkedinIn,
-    FaDAndD,
-    FaSteam
-} from "react-icons/fa";
-import {useSession} from "next-auth/client";
+import * as ReactIcons from "react-icons/fa";
+import {FaAt, FaPhone, FaMapPin} from "react-icons/fa";
+
 
 export default function Footer() {
-   const [ contact , setContact ] = useState()
+   const [ contact , setContact ] = useState([])
+
+    function SocialList() {
+        const elems = [];
+        for (const social of (contact.social || [])) {
+            const Icon = React.createElement(ReactIcons[social.icon], null);
+            elems.push(<li key={social.icon}><a target="_blank" href={social.link}>{Icon}</a></li>)
+        }
+        return elems;
+    }
 
     // Fetch content from protected route
     useEffect(()=>{
-        console.log('useEffect called');
         axios.get('./contact-info.json')
             .then(response => {
-                console.log(response)
                 setContact(response.data)
             })
     }, [])
@@ -33,19 +33,11 @@ export default function Footer() {
                 <Row>
                     <Col></Col>
                     <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col>
+                    <Col><p className={styles.ftInfo}><FaAt/>&nbsp;{contact.email}<br/><FaPhone/>&nbsp;{contact.phone}</p></Col>
+                    <Col><p className={styles.ftInfo}><FaMapPin/>&nbsp;{(contact.address || {}).street}<br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{(contact.address || {}).city}, {(contact.address || {}).state}<br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{(contact.address || {}).zip}</p></Col>
+                    <Col className="align-items-center">
                         <ul className={styles.SocialNetwork}>
-                            <li><a target="_blank" href="https://www.facebook.com/sstoves"><FaFacebookF/></a></li>
-                            <li><a target="_blank" href="https://www.linkedin.com/in/seanstoves"><FaLinkedinIn/></a>
-                            </li>
-                            <li><a target="_blank" href="https://twitter.com/sstoves"><FaTwitter/></a></li>
-                            <li><a target="_blank" href="https://github.com/SeanStoves"><FaGithub/></a></li>
-                            <li><a target="_blank" href="https://www.dndbeyond.com/members/SolaoBajiuik"><FaDAndD/></a>
-                            </li>
-                            <li><a target="_blank" href="https://twitch.tv/solaobajiuik"><FaTwitch/></a></li>
-                            <li><a target="_blank" href="https://steamcommunity.com/id/solaobajiuik"><FaSteam/></a></li>
+                            <SocialList />
                         </ul>
                     </Col>
                 </Row>
