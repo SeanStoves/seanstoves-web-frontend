a8907d337c3fde2dc628579e1f205148c02d3f00
Sean Stoves <sstoves@fandom.com>
2021-09-02 13:42:09 -0400

    Adding placehold.it to the domain list for images

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,24 +1,44 @@
 import Layout from '../../components/design/layout'
+import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Page () {
-    return (
+        return (
         <Layout>
-            <h1>Blog Post 1</h1>
-            <p>
-                Short Description ... read more link
-            </p>
-            <h1>Blog Post 2</h1>
-            <p>
-                Short Description ... read more link
-            </p>
-            <h1>Blog Post 3</h1>
-            <p>
-                Short Description ... read more link
-            </p>
-            <h1>Blog Post 4</h1>
-            <p>
-                Short Description ... read more link
-            </p>
+            <div className="content-block">
+                <div className="post-thubnail">
+                    <Link href="/blog/read?id=1">
+                        <Image src="http://placehold.it/800x400?text=800x400" width="800" height="400" fluid="" alt=""/>
+                    </Link>
+                </div>
+
+                <div className="post-content">
+                    <div className="blog-meta">
+                        <span className="date">2021-02-03</span>
+
+                        <span className="author">By
+                            <Link className="author-name" href="/author/solao">
+                                Solao
+                            </Link>
+                        </span>
+
+
+                        <span className="category">
+                            <Link href="/category/Category-1">
+                               Category 1
+                            </Link>
+                        </span>
+
+                    </div>
+                    <h3 className="title"><Link href="/blog/read?id=2">Blog 2</Link></h3>
+                    <p className="description">description text</p>
+                    <div className="read-more">
+                        <Link href="/blod/read?id=3">
+                            <span>Read More</span>
+                        </Link>
+                    </div>
+                </div>
+            </div>
         </Layout>
     )
 }
