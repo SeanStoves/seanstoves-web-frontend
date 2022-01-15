2ab510b1c7c9ca3bab13da275ba6399cecf0cbb1
Sean Stoves <sean@seanstoves.com>
2022-01-15 18:07:03 -0500

    Updating

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,27 @@
-import Layout from '../components/layout/layout'
+import * as React from 'react';
+import type { NextPage } from 'next';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 
-export default function Page () {
-  return (
-    <Layout>
-      <h1>NextAuth.js Example</h1>
-      <p>
-        This is an example site to demonstrate how to use <a href={`https://next-auth.js.org`}>NextAuth.js</a> for authentication.
-      </p>
-    </Layout>
-  )
-}
+const Home: NextPage = () => {
+    return (
+        <Container maxWidth="lg">
+            <Box
+                sx={{
+                    my: 4,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}
+            >
+                <Typography variant="h4" component="h1" gutterBottom>
+                    MUI v5 + Next.js with TypeScript example
+                </Typography>
+            </Box>
+        </Container>
+    );
+};
+
+export default Home;
