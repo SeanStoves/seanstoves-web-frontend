16732a7d1fe228e2cb9b72efe64f8e51262bc11f
Sean Stoves <sean@seanstoves.com>
2022-01-19 21:45:26 -0500

    Adding age handling constant

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,11 @@ import type { NextPage } from 'next';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import moment from "moment";
 
 const Home: NextPage = () => {
+    const age = (birthday: string) => moment().diff(moment(birthday), 'years');
+
     return (
         <Container maxWidth="lg">
             <Box
@@ -17,11 +20,22 @@ const Home: NextPage = () => {
                 }}
             >
                 <Typography variant="h4" component="h1" gutterBottom>
-                    MUI v5 + Next.js with TypeScript example
-                    <br/>
-                    <br/>
-                    <br/>
+                    Welcome to my page!
                 </Typography>
+            </Box>
+            <Box
+                sx={{
+                    my: 4,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'left',
+                    alignItems: 'left',
+                }}
+            >
+                <Typography variant="body1" component="span">
+                    Hey, Welcome to the website of Sean Stoves Sr a.k.a. Solao Bajiuik.  I am a proud father of 3 young lads aged {age("20091213")}, {age("20100923")} and {age("20131124")}
+                </Typography>
+
             </Box>
         </Container>
     );
