eb4071eef678fbf28f22bd259ad6aeb1dee3f523
Kevin Romero <kevkde@gmail.com>
2024-11-16 19:46:15 -0600

    Initial setup

---
+++ src/app/page.js
@@ -0,0 +1,19 @@
+import FooterComponent from "./components/Footer";
+import NavbarComponent from "./components/Navbar";
+
+export default function Home() {
+  return (
+    <>
+      <NavbarComponent />
+  
+        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+          
+          <br/>
+          <br/>
+          <br/>
+          
+        </main>
+        <FooterComponent />
+    </>
+  );
+}
